bfd19795cb8f99d0ad09aaea791720ab117668d3
eman-name <eman.owusu@gmail.com>
2019-02-23 23:08:47 +0000

    Sync LeetCode submission - Two Sum (java)

---
+++ problems/two_sum/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.get(target-nums[i]) != null){
+                return new int[]{map.get(target-nums[i]), i};
+            }
+            map.put(nums[i],i);
+        }
+        
+        return new int[]{-1,-1};
+    }
+}
