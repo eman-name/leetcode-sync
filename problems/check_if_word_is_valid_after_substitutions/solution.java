d3bc3358e7a30d0c80156528f26710eee5ef986a
eman-name <eman.owusu@gmail.com>
2019-03-08 17:28:31 +0000

    Sync LeetCode submission - Check If Word Is Valid After Substitutions (java)

--- problems/check_if_word_is_valid_after_substitutions/solution.java
+++ problems/check_if_word_is_valid_after_substitutions/solution.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean isValid(String S) {
         Deque<Character> stack = new ArrayDeque<Character>();
         
-        for(char c : S.toCharArray())  {
+        for(char c: S.toCharArray())  {
             if(stack.size() >= 2 && c == 'c' && stack.peek() == 'b') {
                 stack.pop();
-                stack.pop();
+                if(stack.peek() == 'a') stack.pop();
+                else { stack.push('b'); stack.push(c); }
             } else stack.push(c);
         }
         
